4bd63e2ff17215c0b5c7f044805deae1350f7e2f
Ahmed-Hassan-Hassanien <ahmedhassan32681ah@gmail.com>
2025-05-14 19:31:38 +0300

    make the buffer global in common.c instead of making it local in the functions

--- src/common.c
+++ src/common.c
@@ -2,8 +2,6 @@
 
 char buffer[1024];
 void send_to(SOCKET sock){
-    char buffer[1024];
-
     printf("You: ");
     fgets(buffer, sizeof(buffer), stdin);
     send(sock, buffer, strlen(buffer), 0);
@@ -11,8 +9,6 @@ void send_to(SOCKET sock){
 }
 
 void receive(SOCKET sock){
-    char buffer[1024];
-
     memset(buffer, 0, sizeof(buffer));
     recv(sock, buffer, sizeof(buffer), 0);
     printf("%s\n", buffer);
